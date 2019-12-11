658c505791035e83c76d1208d626ecbbea684c62
Evina88 <evinamus@gmail.com>
2019-12-11 13:16:50 +0200

    corrected y.js

--- Week1/y.js
+++ Week1/y.js
@@ -1,10 +1,10 @@
 //declare var y, initialize with string, try to change it, predict and check the result.
 
-let y="string";
+var y = "string";
 console.log('the value of my string will be: string');
 console.log(y);
 
-let y="string2";
-console.log('the value of my string will be: string');
+y = "string2";
+console.log('the value of my string will be: string2');
 console.log(y);
 
